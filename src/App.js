f488d1df386e7d01fb9e49c5fac3a2d2cc126ade
meriam <meriam.hamdaoui@gmail.com>
2023-09-04 15:39:35 +0100

    react router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Notes from "./components/Notes";
+import CreateNote from "./components/CreateNote";
+
 function App() {
-  return <div className="App">my notes react app with mui</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Notes />} />
+      <Route path="/create-note" element={<CreateNote />} />
+    </Routes>
+  );
 }
 
 export default App;
