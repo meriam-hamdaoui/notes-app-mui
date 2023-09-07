64fded00d8a1ebe06a64ee0bb7d6c23f1230c7e5
meriam <meriam.hamdaoui@gmail.com>
2023-09-07 21:56:20 +0100

    axios and useEffect

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,36 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Container, Typography, Card } from "@mui/material";
 
 export default function Notes() {
+  const [data, setData] = useState([]);
+
+  const displayNotes = async () =>
+    await axios
+      .get("http://localhost:8000/notes")
+      .then((res) => {
+        console.log(res.data);
+        setData([...res.data]);
+      })
+      .catch((err) => console.log("err"));
+
+  useEffect(() => {
+    displayNotes();
+  }, []);
+
   return (
-    <Typography variant="h2" color="textSecondary">
-      My Notes
-    </Typography>
+    <Container>
+      <Typography variant="h2" color="textSecondary">
+        My Notes
+      </Typography>
+      {data &&
+        data.map((el) => (
+          <Card key={el.id} variant="div">
+            <Typography variant="h5">{el.title}</Typography>
+            <Typography variant="p">{el.details} </Typography>
+            <Typography variant="h6">{el.category}</Typography>
+          </Card>
+        ))}
+    </Container>
   );
 }
